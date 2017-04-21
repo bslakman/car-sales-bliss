28b663b164ddb84bbd943d0e714d22a6bf13a256
Belinda Slakman <slakman.b@husky.neu.edu>
2017-04-20 20:38:15 -0700

    Week 4 Capstone Update
    Includes updates from Weeks 3 & 4:
    Adding similar vehicles using KNeighbors
    Making regional plots into Bokeh plots

--- cars_app.py
+++ cars_app.py
@@ -2,6 +2,7 @@ from craigslist import *
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
+from bokeh.embed import components
 
 from io import BytesIO
 from flask import Flask, render_template, request, redirect, send_file
@@ -18,20 +19,35 @@ def index():
         app.vars['model'] = request.form['Model']
         app.vars['year'] = request.form['Year']
         app.vars['mileage'] = request.form['Mileage']
+        app.vars['status'] = request.form['Title Status']
+        app.vars['color'] = request.form['Color']
+        app.vars['type'] = request.form['Body Type']
 
         return redirect('/result')
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
-    return render_template('car_result.html')
+    if request.method=='GET':
+        graph = draw_regional_fig_bokeh(app.vars['make'], app.vars['model'], app.vars['year'])
+        script, div = components(graph)
 
-@app.route('/fig/')
-def fig():
-    fig = draw_regional_fig(app.vars['make'], app.vars['model'], app.vars['year'])
-    img = BytesIO()
-    fig.savefig(img)
-    img.seek(0)
-    return send_file(img, mimetype='image/png')
+        price, similar = predict(make=app.vars['make'], model=app.vars['model'],
+            year=app.vars['year'], mileage=app.vars['mileage'], title_status=app.vars['status'],
+            color=app.vars['color'], body_type=app.vars['type'])
+
+        price = '${}'.format(int(round(price)))
+        similar_html = ''
+        for car in similar:
+            desc = car[0]
+            link = car[1]
+            car_price = car[2]
+            similar_html += '<a href="https://boston.craigslist.org/{0}">{1}</a> ${2}<br>'.format(
+                link, desc, int(car_price))
+
+        return render_template('car_result.html', script=script, div=div, similar_html=similar_html, price=price)
+    else:
+        return redirect('/index')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 33507))
+    app.run(port=port)
--- craigslist.py
+++ craigslist.py
@@ -1,30 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
 import re
+import os.path
+from ediblepickle import checkpoint
+
+from bokeh.charts import Bar
+from bokeh.layouts import row
+from bokeh.models import LinearAxis, HoverTool
+from bokeh.models.ranges import Range1d
+from bokeh.plotting import figure, show
+
+from sklearn.externals import joblib
+
+cache_dir = 'cache'
+if not os.path.exists(cache_dir):
+    os.mkdir(cache_dir)
+
+region_dict = {'cap': 'Cape Cod/Islands',
+'har': 'Hartford',
+'new': 'Eastern CT',
+'pro': 'Rhode Island',
+'sou': 'South Coast',
+'wor': 'Worcester/Central Mass',
+'bmw': 'Boston Metro West',
+'gbs': 'Boston/Cambridge/Brookline',
+'nos': 'North Shore',
+'nwb': 'Northwest/Merrimack',
+'sob': 'South Shore'}
 
 def fetch(query = None, auto_make_model = None, min_auto_year = None, max_auto_year = None, s=0):
     search_params = {key: val for key, val in locals().items() if val is not None}
-    if not search_params: 
+    if not search_params:
         raise ValueError("No valid keywords")
-        
+
     base = "http://boston.craigslist.org/search/cto"
     resp = requests.get(base, params=search_params, timeout=3)
     resp.raise_for_status()
     return resp.content, resp.encoding
 
-
-# In[4]:
-
 def parse(html, encoding='utf-8'):
     parsed = BeautifulSoup(html, 'lxml', from_encoding=encoding)
     return parsed
-
-
-# In[5]:
 
 def extract_listings(parsed):
     listings = parsed.find_all('p', class_='result-info')
@@ -101,17 +123,17 @@ def draw_regional_fig(make, model, year):
         car_results = fetch(auto_make_model=make_model, min_auto_year=min_auto_year, max_auto_year=max_auto_year, s=i)
         doc = parse(car_results[0])
         listings.extend(extract_listings(doc))
-    
+
     df = pd.DataFrame(data=listings)
     if len(df) == 0: return "No results found, check your spelling"
     df['mileage'] = df.apply(lambda row: get_mileage(row['description']), axis=1)
     df['price'] = df.apply(lambda row: get_price(row['price']), axis=1)
     df['region'] = df['link'].str[1:5]
     df['year'] = df.apply(lambda row: get_year(row['description']), axis=1)
-    
+
     regions = df.groupby('region').mean()
     regions = regions.append(pd.Series(data={'year': np.mean(df['year']), 'price': np.mean(df['price']), 'mileage': np.mean(df['mileage'])}, name='AVERAGE'))
-    
+
     sns.set_style('ticks')
     my_title = 'Average Price and Mileage of Used {0} {1}, \n{2}-{3}, by region, n={4}'.format(make, model, min_auto_year, max_auto_year, len(df))
     ax = regions['price'].plot.bar(position=0, width=0.3, alpha=0.5, legend=True, title=my_title, figsize=(5,3))
@@ -120,6 +142,69 @@ def draw_regional_fig(make, model, year):
     ax.set_ylabel('Mileage')
     sns.despine(top=True, right=False)
     fig=ax.get_figure()
-    fig.set_tight_layout(True) 
+    fig.set_tight_layout(True)
     return fig
 
+def draw_regional_fig_bokeh(make, model, year):
+    try:
+        min_auto_year = int(year) - 2
+        max_auto_year = int(year) + 2
+    except:
+        min_auto_year = 2012
+        max_auto_year = 2016
+
+    listings = get_listings(make, model, year)
+
+    df = pd.DataFrame(data=listings)
+    if len(df) == 0: return "No results found, check your spelling"
+    df['mileage'] = df.apply(lambda row: get_mileage(row['description']), axis=1)
+    df['price'] = df.apply(lambda row: get_price(row['price']), axis=1)
+    df['region'] = df['link'].str[1:5]
+    df['year'] = df.apply(lambda row: get_year(row['description']), axis=1)
+    df['full_region'] = df.apply(lambda row: region_dict[row['region'].strip('/')], axis=1)
+    mileage_count = len(df[df['mileage'].notnull()])
+
+    title1 = 'Average Price of Used {0} {1}, {2}-{3}, by region, n={4}'.format(make, model, min_auto_year, max_auto_year, len(df))
+    title2 = 'Average Mileage of Used {0} {1}, {2}-{3}, by region, n={4}'.format(make, model, min_auto_year, max_auto_year, mileage_count)
+    bar1 = Bar(df, label='full_region', values='price', agg='mean',
+        title=title1, legend=None, xlabel="Region", ylabel="Price", y_range=(0, 20000))
+    bar2 = Bar(df, label='full_region', values='mileage', agg='mean',
+        title=title2, legend=None, xlabel="Region", ylabel="Mileage", y_range=(0, 150000), color='dodgerblue')
+    return row(bar1, bar2)
+
+@checkpoint(key=lambda args, kwargs: "-".join(args) + '.p', work_dir=cache_dir)
+def get_listings(make,model,year):
+    listings = []
+    make_model = "{0} {1}".format(make,model)
+    try:
+        min_auto_year = int(year) - 2
+        max_auto_year = int(year) + 2
+    except:
+        min_auto_year = 2012
+        max_auto_year = 2016
+    if max_auto_year > 2016:
+        max_auto_year = 2016
+    for i in range(0, 500, 100):
+        car_results = fetch(auto_make_model=make_model, min_auto_year=min_auto_year, max_auto_year=max_auto_year, s=i)
+        doc = parse(car_results[0])
+        listings.extend(extract_listings(doc))
+    return listings
+
+def predict(make='', model='', year='', mileage='', title_status='', color='', body_type=''):
+    X_test = [{'make': make.lower(),
+             'model': model.lower(),
+             'year': int(year) if year else 0,
+             'odometer': int(mileage) if mileage else 0,
+             'title status': title_status,
+             'paint color': color,
+             'type': body_type,
+             }]
+    v = joblib.load('Cars/dv.pkl')
+    X_trans = v.transform(X_test)
+    kn = joblib.load('Cars/kneighbors_kn.pkl')
+    all_filtered = joblib.load('Cars/all_filtered.pkl')
+    price = kn.predict(X_trans[0])
+    neighbors_info = []
+    for j in kn.kneighbors(X_trans[0])[1][0][:10]:
+        neighbors_info.append((all_filtered.iloc[j]['description'],all_filtered.iloc[j]['link'],all_filtered.iloc[j]['price']))
+    return price[0], neighbors_info
